9cd754e9477576e13e72cbbe2421967e0ffcbd5f
Lucky Dewangan <luckydewangan791@gmail.com>
2023-03-15 19:25:10 +0530

    improved UI and added error handling

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require('dotenv').config
 const express = require("express");
 const https = require("https");   
 const bodyParser = require("body-parser");
 const app = express();
 require('dotenv').config();
+const path = require('path')
+const axios = require('axios');
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 const API_KEY = process.env.API_KEY;
@@ -12,33 +17,67 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 // handling get request
 app.get("/", function (req, res) {
+    console.log("get recieved")
     res.sendFile(__dirname + "/index.html");
 })
 
+var options = {
+    hostname: 'encrypted.google.com',
+    port: 443,
+    path: '/',
+    method: 'GET'
+  };
+
 // handling post request
-app.post("/", function (req, res) {
+app.post("/", async (req, res) => {
     const query = req.body.cityName;
     const unit = "metric";
     const appKey = `${API_KEY}`;
     let url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid="+ appKey + "";
 
     // A native node module to make http request(here it is get request) to another server 
-    https.get(url, function(response){
-           
-    response.on("data", function (data) {
-        weatherData = JSON.parse(data);
-        const temp = weatherData.main.temp;
-        const city = weatherData.name;
-        const icon = weatherData.weather[0].icon;
-        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-        const description = weatherData.weather[0].description;
-        res.render('report', {temperature: temp, imgUrl: imageURL, descrpt: description, cityName: city});
-    })
-})
+
+    try {
+		const response = await axios({
+			url: url,
+			method: "get",
+		});
+
+        // res.status(200).json(response.data);
+		    const weatherData = response.data;
+            // console.log(response.data)
+            // res.send("we recieved your request")
+            const temp = weatherData.main.temp;
+            const city = weatherData.name;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const description = weatherData.weather[0].description;
+            res.render('report', {temperature: temp, imgUrl: imageURL, descrpt: description, cityName: city});
+	} catch (err) {
+        res.status(500).render('error', {errorMessage: err});
+	}
+
+    // https.get(url, function(response){
+       
+    //     response.on("data", function (data) {
+
+            
+    //         weatherData = JSON.parse(data);
+    //         const temp = weatherData.main.temp;
+    //         const city = weatherData.name;
+    //         const icon = weatherData.weather[0].icon;
+    //         const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    //         const description = weatherData.weather[0].description;
+    //         res.render('report', {temperature: temp, imgUrl: imageURL, descrpt: description, cityName: city});
+    //     }) 
+    // }).on('error', (e)=>{
+    //     console.error(e);
+    // })
+    
 })
 
 // listen to the server at port 3000
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
     console.log("server started");
 })
 
